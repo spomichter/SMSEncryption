f5a3c99b0726801f843b320a06e5c4e129212d43
Daniel Winston <disabledfishinabarrel@gmail.com>
2016-11-11 12:21:31 -0800

    SMS sends, still adding visual functionality

--- app/src/main/java/tk/greatdysonia/smsencryption/MainActivity.java
+++ app/src/main/java/tk/greatdysonia/smsencryption/MainActivity.java
@@ -1,13 +1,28 @@
 package tk.greatdysonia.smsencryption;
 
+// If you tryna buy the half-o, you don't want the piggy to know.
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.*;
 
 public class MainActivity extends AppCompatActivity {
+
+private static final int PERMISSIONS_SEND_SMS = 1;
+    private String recipientNumber = "";
+    private String msg = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, PERMISSIONS_SEND_SMS);
+        }
+        else {
+            //SmsManager.getDefault().sendTextMessage(recipientNumber, null, msg, null,null);
+        }
     }
 }
